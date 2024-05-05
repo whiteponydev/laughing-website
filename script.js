964001b732c78d8938a0b4053e4d6bfe8654c723
Sonia Naranjo <sonianaranjo66@gmail.com>
2024-05-05 15:32:44 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const enter = document.getElementById('enter');
+const box = document.getElementById('box');
+
+enter.addEventListener('click', () => {
+    enter.remove();
+
+    setInterval(checkTime, 60000);
+
+    function checkTime() {
+        const d = new Date();
+        const h = d.getHours();
+        const m = d.getMinutes();
+        console.log(d);
+        console.log(h);
+        console.log(m);
+
+        if (h === 15 && m === 40){
+            const laugh = document.createElement('audio');
+            laugh.src = 'sounds/laughing.wav';
+            laugh.type = 'audio/wav';
+            box.appendChild(laugh);
+            laugh.play();
+        }    
+    }
+})
